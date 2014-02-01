ea6e479fd5e87ebb836796ea317849f7f1697396
unknown <p2-1020@P2-1020-17.laboratoire.uqac.ca>
2014-01-31 21:17:29 -0500

    version 2

--- Exercice no3/ex3team1.py
+++ Exercice no3/ex3team1.py
@@ -1,28 +1,26 @@
 
 def relationDInfluence(nbRelation, tabRelation):
-	if nbRelation != len(tabRelation):
+    if nbRelation != len(tabRelation):
 		print('Input incorrecte')
-	plusLongueRelation
-	for x in nbRelation:
-		pivot = tabRelation[x].partition(' ')[2]
-		plusLongueRelation[x] = 1
-		y = x+1
-		while y <= n:
-			if tabRelation[y].partition(' ')[2] == pivot:
-				plusLongueRelation[x] = plusLongueRelation[x] + 1
-				pivot = tabRelation[y].partition(' ')[2]
+    else:
+        plusLongueRelation = [None] * nbRelation
+        for x in range(0, nbRelation):
+            pivot = tabRelation[x].partition('_')[2]
+            y = x+1
+            for n in range(0, nbRelation-1):
+                pivot = tabRelation[x][0]
+            	if tabRelation[n][0] == pivot:
+                   plusLongueRelation[x] = pivot
+                   pivot = tabRelation[y].partition(' ')[2]
+    	plusLongueRelation.sort()
+    	print plusLongueRelation
 
 
-	nbRelMax = plusLongueRelation.sort()
-	
-
-	print(nbRelMax)
 
 
-def main():
-	nbRelation = 5
-	tabRelation = {1 2, 1 3, 2 4, 3 4, 1 4}
-	relationDInfluence(nbRelation, tabRelation)
+nbRelation = 5
+tabRelation ="1_2", "2_3", "3_4", "2_5", "5_6"
+relationDInfluence(nbRelation, tabRelation)
 
 def recursiveFunc(a, b, taille, nbMax, tab):
 	if n > 0:
